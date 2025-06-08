800350688c8aa02a88a543f828b156da4764e35c
rgs2151 <rgs2151@columbia.edu>
2025-06-08 17:53:43 -0400

    vocal extractions

--- data/movie/audio.py
+++ data/movie/audio.py
@@ -6,12 +6,7 @@ import glob
 import numpy as np
 import librosa
 from types import SimpleNamespace
-# from dataclasses import dataclass
-
-# @dataclass
-# class Source:
-#     volume: Dataset
-#     timbre:  Dataset
+import pandas as pd
 
 class Audio(Dataset):
     # Raw audio is sampled at 48000 Hz
@@ -32,7 +27,32 @@ class Audio(Dataset):
     @property
     def vocals(self):
         volume, timbre = calculate_volume_and_timbre(os.path.join(self._base_path, "../bin/movie/audio/vocals.wav"))
-        return SimpleNamespace(volume=volume, timbre=timbre)
+        
+        # According to 3loi/SER-Odyssey-Baseline-WavLM-Multi-Attributes
+        vocal_regress = np.load(os.path.join(self._base_path, "../bin/movie/audio/vocal_attributes.npy"))
+        arousal = Dataset.from_array(vocal_regress[:, 0], 0.3)
+        dominance = Dataset.from_array(vocal_regress[:, 1], 0.3)
+        valence = Dataset.from_array(vocal_regress[:, 2], 0.3)
+        
+        # According to 3loi/SER-Odyssey-Baseline-WavLM-Categorical-Attributes
+        # {0: 'Angry',1: 'Sad',2: 'Happy',3: 'Surprise',4: 'Fear',5: 'Disgust',6: 'Contempt',7: 'Neutral'}
+        emotions = pd.read_parquet(os.path.join(self._base_path, "../bin/movie/audio/vocal_emos.parquet"))
+        
+        # make seperate array for each emotion
+        angry = Dataset.from_array(emotions['Angry'].to_numpy(), 0.3)
+        sad = Dataset.from_array(emotions['Sad'].to_numpy(), 0.3)
+        happy = Dataset.from_array(emotions['Happy'].to_numpy(), 0.3)
+        surprise = Dataset.from_array(emotions['Surprise'].to_numpy(), 0.3)
+        fear = Dataset.from_array(emotions['Fear'].to_numpy(), 0.3)
+        disgust = Dataset.from_array(emotions['Disgust'].to_numpy(), 0.3)
+        contempt = Dataset.from_array(emotions['Contempt'].to_numpy(), 0.3)
+        neutral = Dataset.from_array(emotions['Neutral'].to_numpy(), 0.3)
+        
+        return SimpleNamespace(volume=volume, timbre=timbre, 
+                               arousal=arousal, dominance=dominance, valence=valence, 
+                               angry=angry, sad=sad, happy=happy, surprise=surprise, 
+                               fear=fear, disgust=disgust, contempt=contempt, 
+                               neutral=neutral)
     
         
         
